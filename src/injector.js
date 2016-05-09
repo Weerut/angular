b163153400d381f129b2163e675fc35eabbce5fa
weerut.panjawattana <weerut.panjawattana@thomsonreuters.com>
2016-05-09 08:29:22 +0700

    Chapter11-StirctMode

---
+++ src/injector.js
@@ -0,0 +1,72 @@
+'use strict';
+
+var _ = require('lodash');
+
+var FN_ARGS = /^function\s*[^\(]*\(\s*([^\)]*)\)/m;
+var FN_ARG = /^\s*(_?)(\S+?)\1\s*$/;
+var STRIP_COMMENTS = /(\/\/.*$)|(\/\*.*?\*\/)/mg;
+
+function createInjector(modulesToLoad) {
+	var cache = {};
+	var loadedModules = {};
+	var $provide = {
+		constant: function(key, value) {
+			if (key === 'hasOwnProperty') {
+				throw 'hasOwnProperty is not a valid constant name!';
+			}
+			cache[key] = value;
+		}
+	};
+
+	function invoke(fn, self, locals) {
+		var args = _.map(fn.$inject, function(token) {
+			if (_.isString(token)) {
+				return locals && locals.hasOwnProperty(token) ? locals[token] : cache[token];
+			} else {
+				throw 'Incorrect injection token! Expected a string, got ' + token;
+			}
+		});
+		return fn.apply(self, args);
+	}
+
+	function annotate(fn) {
+		if (_.isArray(fn)) {
+			return fn.slice(0, fn.length - 1);
+		} else if (fn.$inject) {
+			return fn.$inject;
+		} else if (!fn.length) {
+			return [];
+		} else {
+			var source = fn.toString().replace(STRIP_COMMENTS, '');
+			var argDeclaration = fn.toString().match(FN_ARGS);
+			return _.map(argDeclaration[1].split(','), function(argName) {
+				return argName.match(FN_ARG)[2];
+			});
+		}
+	}
+
+	_.forEach(modulesToLoad, function loadModule(moduleName) {
+		if (!loadedModules.hasOwnProperty(moduleName)) {
+			loadedModules[moduleName] = true;
+			var module = window.angular.module(moduleName);
+			_.forEach(module.requires, loadModule);
+			_.forEach(module._invokeQueue, function(invokeArgs) {
+				var method = invokeArgs[0];
+				var args = invokeArgs[1];
+				$provide[method].apply($provide, args);
+			});
+		}
+	});
+	return {
+		has: function(key) {
+			return cache.hasOwnProperty(key);
+		},
+		get: function(key) {
+			return cache[key];
+		},
+		annotate: annotate,
+		invoke: invoke
+	};
+}
+
+module.exports = createInjector;
