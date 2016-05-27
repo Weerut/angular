1265a3d621514e7c7fcd7676c85f37d07db75ca1
weerut.panjawattana <weerut.panjawattana@thomsonreuters.com>
2016-05-27 16:33:07 +0700

    Chapter13-Begin

--- src/injector.js
+++ src/injector.js
@@ -9,9 +9,54 @@ var INSTANTIATING = {};
 
 function createInjector(modulesToLoad, strictDi) {
 	var providerCache = {};
+	var providerInjector = createInternalInjector(providerCache, function() {
+		throw 'Unknown provider: ' + path.join(' <- ');
+	});
 	var instanceCache = {};
+	var instanceInjector = createInternalInjector(instanceCache, function(name) {
+		var provider = providerInjector.get(name + 'Provider');
+		return instanceInjector.invoke(provider.$get, provider);
+	});
 	var loadedModules = {};
 	var path = [];
+	strictDi = (strictDi === true);
+
+	var $provide = {
+		constant: function(key, value) {
+			if (key === 'hasOwnProperty') {
+				throw 'hasOwnProperty is not a valid constant name!';
+			}
+			providerCache[key] = value;
+			instanceCache[key] = value;
+		},
+		provider: function(key, provider) {
+			if (_.isFunction(provider)) {
+				provider = providerInjector.instantiate(provider);
+			}
+			providerCache[key + 'Provider'] = provider;
+		}
+	};
+
+	// Get array list of function's paramter name.
+	function annotate(fn) {
+		if (_.isArray(fn)) {
+			return fn.slice(0, fn.length - 1);
+		} else if (fn.$inject) {
+			return fn.$inject;
+		} else if (!fn.length) {
+			return [];
+		} else {
+			if (strictDi) {
+				throw 'fn is not using explicit annotation and ' +
+					'cannot be invoked in strict mode';
+			}
+			var source = fn.toString().replace(STRIP_COMMENTS, '');
+			var argDeclaration = fn.toString().match(FN_ARGS);
+			return _.map(argDeclaration[1].split(','), function(argName) {
+				return argName.match(FN_ARG)[2];
+			});
+		}
+	}
 
 	function createInternalInjector(cache, factoryFn) {
 		function getService(name) {
@@ -34,91 +79,44 @@ function createInjector(modulesToLoad, strictDi) {
 				}
 			}
 		}
-	}
 
-
-	strictDi = (strictDi === true);
-	var $provide = {
-		constant: function(key, value) {
-			if (key === 'hasOwnProperty') {
-				throw 'hasOwnProperty is not a valid constant name!';
-			}
-			instanceCache[key] = value;
-		},
-		provider: function(key, provider) {
-			if (_.isFunction(provider)) {
-				provider = instantiate(provider);
-			}
-			providerCache[key + 'Provider'] = provider;
-		}
-
-	};
-
-	// function getService(name) {
-	// 	if (instanceCache.hasOwnProperty(name)) {
-	// 		if (instanceCache[name] === INSTANTIATING) {
-	// 			throw new Error('Circular dependency found: ' +
-	// 				name + ' <- ' + path.join(' <- '));
-	// 		}
-	// 		return instanceCache[name];
-	// 	} else if (providerCache.hasOwnProperty(name)) {
-	// 		return providerCache[name];
-	// 	} else if (providerCache.hasOwnProperty(name + 'Provider')) {
-	// 		path.unshift(name);
-	// 		instanceCache[name] = INSTANTIATING;
-	// 		try {
-	// 			var provider = providerCache[name + 'Provider'];
-	// 			var instance = instanceCache[name] = invoke(provider.$get, provider);
-	// 			return instance;
-	// 		} finally {
-	// 			path.shift(name);
-	// 			if (instanceCache[name] === INSTANTIATING) {
-	// 				delete instanceCache[name];
-	// 			}
-	// 		}
-	// 	}
-	// }
-
-	function invoke(fn, self, locals) {
-		var args = _.map(annotate(fn), function(token) {
-			if (_.isString(token)) {
-				return locals && locals.hasOwnProperty(token) ?
-					locals[token] : getService(token);
-			} else {
-				throw 'Incorrect injection token! Expected a string, got ' + token;
-			}
-		});
-		if (_.isArray(fn)) {
-			fn = _.last(fn);
-		}
-		return fn.apply(self, args);
-	}
-
-	function annotate(fn) {
-		if (_.isArray(fn)) {
-			return fn.slice(0, fn.length - 1);
-		} else if (fn.$inject) {
-			return fn.$inject;
-		} else if (!fn.length) {
-			return [];
-		} else {
-			if (strictDi) {
-				throw 'fn is not using explicit annotation and ' +
-					'cannot be invoked in strict mode';
-			}
-			var source = fn.toString().replace(STRIP_COMMENTS, '');
-			var argDeclaration = fn.toString().match(FN_ARGS);
-			return _.map(argDeclaration[1].split(','), function(argName) {
-				return argName.match(FN_ARG)[2];
+		function invoke(fn, self, locals) {
+			//  Get value of parameter in parameter array.
+			var args = _.map(annotate(fn), function(token) {
+				if (_.isString(token)) {
+					return locals && locals.hasOwnProperty(token) ?
+						locals[token] : getService(token);
+				} else {
+					throw 'Incorrect injection token! Expected a string, got ' + token;
+				}
 			});
+			// If original function is array, then extract only function part
+			if (_.isArray(fn)) {
+				fn = _.last(fn);
+			}
+			// Apply that function and return function value.
+			return fn.apply(self, args);
 		}
-	}
 
-	function instantiate(Type, locals) {
-		var UnwrappedType = _.isArray(Type) ? _.last(Type) : Type;
-		var instance = Object.create(UnwrappedType.prototype);
-		invoke(Type, instance, locals);
-		return instance;
+		function instantiate(Type, locals) {
+			var UnwrappedType = _.isArray(Type) ? _.last(Type) : Type;
+			var instance = Object.create(UnwrappedType.prototype);
+			invoke(Type, instance, locals);
+			return instance;
+		}
+
+		function has(name) {
+			return cache.hasOwnProperty(name) ||
+				providerCache.hasOwnProperty(name + 'Provider');
+		}
+
+		return {
+			has: has,
+			get: getService,
+			annotate: annotate,
+			invoke: invoke,
+			instantiate: instantiate
+		};
 	}
 
 	_.forEach(modulesToLoad, function loadModule(moduleName) {
@@ -133,16 +131,8 @@ function createInjector(modulesToLoad, strictDi) {
 			});
 		}
 	});
-	return {
-		has: function(key) {
-			return instanceCache.hasOwnProperty(key) ||
-				providerCache.hasOwnProperty(key + 'Provider');
-		},
-		get: getService,
-		annotate: annotate,
-		invoke: invoke,
-		instantiate: instantiate
-	};
+
+	return instanceInjector;
 }
 
 module.exports = createInjector;
