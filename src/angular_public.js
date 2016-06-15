a87eb6c92c0ce46ed03a7e07622d9a3982d3254e
weerut.panjawattana <weerut.panjawattana@thomsonreuters.com>
2016-06-16 02:01:21 +0700

    Text Node Interpolation

--- src/angular_public.js
+++ src/angular_public.js
@@ -16,6 +16,7 @@ function publishExternalAPI() {
 	ngModule.provider('$httpParamSerializerJQLike',
 		require('./http').$HttpParamSerializerJQLikeProvider);
 	ngModule.provider('$compile', require('./compile'));
+	ngModule.provider('$interpolate', require('./interpolate'));
 	ngModule.provider('$controller', require('./controller'));
 	ngModule.directive('ngController', require('./directives/ng_controller'));
 	ngModule.directive('ngTransclude', require('./directives/ng_transclude'));
---
+++ src/interpolate.js
@@ -0,0 +1,61 @@
+'use strict';
+
+var _ = require('lodash');
+
+function stringify(value) {
+	if (_.isNull(value) || _.isUndefined(value)) {
+		return '';
+	} else if (_.isObject(value)) {
+		return JSON.stringify(value);
+	} else {
+		return '' + value;
+	}
+}
+
+function unescapeText(text) {
+	return text.replace(/\\{\\{/g, '{{').replace(/\\}\\}/g, '}}');
+}
+
+function $InterpolateProvider() {
+	this.$get = ['$parse', function($parse) {
+		function $interpolate(text, mustHaveExpressions) {
+			var index = 0;
+			var parts = [];
+			var hasExpressions = false;
+			var startIndex, endIndex, exp, expFn;
+			while (index < text.length) {
+				startIndex = text.indexOf('{{', index);
+				if (startIndex !== -1) {
+					endIndex = text.indexOf('}}', startIndex + 2);
+				}
+				if (startIndex !== -1 && endIndex !== -1) {
+					if (startIndex !== index) {
+						parts.push(unescapeText(text.substring(index, startIndex)));
+					}
+					exp = text.substring(startIndex + 2, endIndex);
+					expFn = $parse(exp);
+					parts.push(expFn);
+					hasExpressions = true;
+					index = endIndex + 2;
+				} else {
+					parts.push(unescapeText(text.substring(index)));
+					break;
+				}
+			}
+			if (hasExpressions || !mustHaveExpressions) {
+				return function interpolationFn(context) {
+					return _.reduce(parts, function(result, part) {
+						if (_.isFunction(part)) {
+							return result + stringify(part(context));
+						} else {
+							return result + part;
+						}
+					}, '');
+				};
+			}
+		}
+		return $interpolate;
+	}];
+}
+
+module.exports = $InterpolateProvider;
